3853191c6691f14c187236863461824feab8a6db
poklipart <angellopezandrade4@gmail.com>
2019-12-12 14:22:48 +1100

    initial commit - project base setup

---
+++ app.js
@@ -0,0 +1,23 @@
+//Sets up application packages/middleware
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost/games_afternoon", {
+    useNewUrlParser: true
+});
+mongoose.Promise = global.Promise;
+mongoose.connection.on("error", (err) => console.log(err));
+
+app.use(methodOverride("_method", { methods: ["POST", "GET"]}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const routes = require("./routes");
+app.use(routes);
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,5 @@
+//Sets up the application with the port
+const app = require("./app");
+const port = 3000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
